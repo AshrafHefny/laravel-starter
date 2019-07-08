e29440f6bc5d6e1c3da698faf865e1a623577bd6
Eslam <eslamaboelwafa@gmail.com>
2019-07-08 09:29:06 +0200

    module config

---
+++ config/modules.php
@@ -0,0 +1,7 @@
+<?php
+return[
+    'users'=>['edit', 'view', 'delete', 'create','reactivate'],
+    'options'=>['create', 'edit', 'view', 'delete'],
+    'translator'=> ['create', 'edit', 'view', 'delete'],
+    'config'=>['edit'],
+];
